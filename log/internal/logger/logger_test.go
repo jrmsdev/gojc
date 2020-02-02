10884cdbf3885bdc3f90f1190b9d9149c2977eb5
Jeremías <jrmsdev@gmail.com>
2020-02-02 02:21:22 -0300

    test internal logger

---
+++ log/internal/logger/logger_test.go
@@ -0,0 +1,185 @@
+// Copyright (c) Jeremías Casteglione <jrmsdev@gmail.com>
+// See LICENSE file.
+
+package logger
+
+import (
+	"bytes"
+	"log"
+	"testing"
+	"strings"
+
+	//~ gfmt "fmt"
+
+	//~ "github.com/jrmsdev/gojc/errors"
+
+	. "github.com/jrmsdev/gojc/testing/check"
+)
+
+var buf *bytes.Buffer
+
+func init() {
+	buf = new(bytes.Buffer)
+}
+
+func check(t *testing.T, match string) {
+	t.Helper()
+	got := strings.TrimSpace(buf.String())
+	buf.Reset()
+	if match != "" {
+		Match(t, match, got, "check")
+	} else {
+		IsEqual(t, got, match, "check")
+	}
+}
+
+func TestLogger(t *testing.T) {
+	New(OFF, false)
+	IsEqual(t, log.Flags(), flagsDefault, "default log flags")
+	IsEqual(t, levelTag[PANIC], "", "tag")
+	IsEqual(t, levelColor[PANIC], red, "color")
+	IsEqual(t, levelTag[FATAL], "", "tag")
+	IsEqual(t, levelColor[FATAL], red, "color")
+}
+
+func TestLoggerOFF(t *testing.T) {
+	oldw := log.Writer()
+	log.SetOutput(buf)
+	defer log.SetOutput(oldw)
+	IsEqual(t, levelTag[OFF], "", "tag")
+	IsEqual(t, levelColor[OFF], white, "color")
+	l := New(OFF, false)
+	l.Print(MSG, "testing")
+	check(t, "")
+	l.Printf(MSG, "testing")
+	check(t, "")
+}
+
+func TestLoggerERROR(t *testing.T) {
+	oldw := log.Writer()
+	log.SetOutput(buf)
+	defer log.SetOutput(oldw)
+	IsEqual(t, levelTag[ERROR], "[E] ", "tag")
+	IsEqual(t, levelColor[ERROR], red, "color")
+	l := New(ERROR, false)
+	l.Print(PANIC, "testing")
+	check(t, `: testing`)
+	l.Print(FATAL, "testing")
+	check(t, `: testing`)
+	l.Print(ERROR, "testing")
+	check(t, `: \[E] testing`)
+	l.Printf(WARN, "testing")
+	check(t, "")
+	l.Printf(MSG, "testing")
+	check(t, "")
+	l.Printf(INFO, "testing")
+	check(t, "")
+	l.Printf(DEBUG, "testing")
+	check(t, "")
+}
+
+func TestLoggerWARN(t *testing.T) {
+	oldw := log.Writer()
+	log.SetOutput(buf)
+	defer log.SetOutput(oldw)
+	IsEqual(t, levelTag[WARN], "[W] ", "tag")
+	IsEqual(t, levelColor[WARN], yellow, "color")
+	l := New(WARN, false)
+	l.Print(PANIC, "testing")
+	check(t, `: testing`)
+	l.Print(FATAL, "testing")
+	check(t, `: testing`)
+	l.Print(ERROR, "testing")
+	check(t, `: \[E] testing`)
+	l.Printf(WARN, "testing")
+	check(t, `: \[W] testing`)
+	l.Printf(MSG, "testing")
+	check(t, "")
+	l.Printf(INFO, "testing")
+	check(t, "")
+	l.Printf(DEBUG, "testing")
+	check(t, "")
+}
+
+func TestLoggerMSG(t *testing.T) {
+	oldw := log.Writer()
+	log.SetOutput(buf)
+	defer log.SetOutput(oldw)
+	IsEqual(t, levelTag[MSG], "", "tag")
+	IsEqual(t, levelColor[MSG], white, "color")
+	l := New(MSG, false)
+	l.Print(PANIC, "testing")
+	check(t, `: testing`)
+	l.Print(FATAL, "testing")
+	check(t, `: testing`)
+	l.Print(ERROR, "testing")
+	check(t, `: \[E] testing`)
+	l.Printf(WARN, "testing")
+	check(t, `: \[W] testing`)
+	l.Printf(MSG, "testing")
+	check(t, ": testing")
+	l.Printf(INFO, "testing")
+	check(t, "")
+	l.Printf(DEBUG, "testing")
+	check(t, "")
+}
+
+func TestLoggerINFO(t *testing.T) {
+	oldw := log.Writer()
+	log.SetOutput(buf)
+	defer log.SetOutput(oldw)
+	IsEqual(t, levelTag[INFO], "[I] ", "tag")
+	IsEqual(t, levelColor[INFO], cyan, "color")
+	l := New(INFO, false)
+	l.Print(PANIC, "testing")
+	check(t, `: testing`)
+	l.Print(FATAL, "testing")
+	check(t, `: testing`)
+	l.Print(ERROR, "testing")
+	check(t, `: \[E] testing`)
+	l.Printf(WARN, "testing")
+	check(t, `: \[W] testing`)
+	l.Printf(MSG, "testing")
+	check(t, ": testing")
+	l.Printf(INFO, "testing")
+	check(t, `: \[I] testing`)
+	l.Printf(DEBUG, "testing")
+	check(t, "")
+}
+
+func TestLoggerDEBUG(t *testing.T) {
+	oldw := log.Writer()
+	log.SetOutput(buf)
+	defer log.SetOutput(oldw)
+	IsEqual(t, levelTag[DEBUG], "[D] ", "tag")
+	IsEqual(t, levelColor[DEBUG], green, "color")
+	l := New(DEBUG, false)
+	l.Print(PANIC, "testing")
+	check(t, `: testing`)
+	l.Print(FATAL, "testing")
+	check(t, `: testing`)
+	l.Print(ERROR, "testing")
+	check(t, `: \[E] testing`)
+	l.Printf(WARN, "testing")
+	check(t, `: \[W] testing`)
+	l.Printf(MSG, "testing")
+	check(t, ": testing")
+	l.Printf(INFO, "testing")
+	check(t, `: \[I] testing`)
+	l.Printf(DEBUG, "testing")
+	check(t, `: \[D] testing`)
+}
+
+func TestLoggerSetLevel(t * testing.T) {
+	l := New(OFF, false).(*L)
+	IsEqual(t, l.lvl, OFF, "off level")
+	l.SetLevel(DEBUG)
+	IsEqual(t, l.lvl, DEBUG, "debug level")
+	if err := l.SetLevel(PANIC); err == nil {
+		t.Error("set level panic did not failed")
+	} else {
+		IsTrue(t, ErrInvalidLevel.Is(err), "invalid error")
+	}
+	l.SetLevel(MSG)
+	IsEqual(t, l.lvl, MSG, "msg level")
+}
