05579f3993f83ff49ea084fd0d2b931bd8d706df
Jeremías <jrmsdev@gmail.com>
2020-02-03 20:37:45 -0300

    bench config GetRaw

---
+++ internal/_bench/bench_config_test.go
@@ -0,0 +1,23 @@
+// Copyright (c) Jeremías Casteglione <jrmsdev@gmail.com>
+// See LICENSE file.
+
+package bench
+
+import (
+	"testing"
+
+	"github.com/jrmsdev/gojc/config"
+)
+
+var bcfg = &config.Cfg{
+	"default": &config.Section{
+		"testing": "ok",
+	},
+}
+
+func BenchmarkConfigGetRaw(b *testing.B) {
+	c := config.Map(bcfg)
+	for i := 0; i < b.N; i++ {
+		c.GetRaw("default", "testing")
+	}
+}
